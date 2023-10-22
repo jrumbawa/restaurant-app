0bec150fea89304623472a9fa7856e21c77a9ab6
jrumbawa <145185249+jrumbawa@users.noreply.github.com>
2023-10-22 13:09:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import { menuArray } from "./data"
+import {menuArray} from "./data.js"
 
 const menu = document.getElementById("menu")
 
-menuArray.map(menuItem => {})
+function renderMenu() {
+  const menuHtml = menuArray.map(menuItem => {
+    return `
+      <div class="menu-item">
+        <div class="menu-icon">${menuItem.emoji}</div>
+        <div class="menu-details">
+          <p>${menuItem.name}</p>
+          <p>${menuItem.ingredients.join(',')}
+          <p>${menuItem.price}</p>
+        </div>
+        <button>+</button>
+      </div>
+    `
+  })
+
+  menu.innerHTML = menuHtml.join('')
+}
+
+renderMenu()
